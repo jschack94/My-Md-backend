2d1207b8b40f198594c5f853498b193490050950
Lindsay Mecher <lindsaymecher@gmail.com>
2020-03-02 17:28:54 -0600

    Added stringify time and date methods on the appointment model.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,6 +2,15 @@ class Appointment < ApplicationRecord
   belongs_to :patient
   belongs_to :doctor
 
-  validates_presence_of :date, :time, :diagnosis, :directions, :doctor_id, :patient_id
+  validates_presence_of :date, :time, :diagnosis, :directions, :doctor_id, :patient_id, :stringified_date, :stringified_time
 
+  def stringified_date
+    string = self.date
+    string.strftime("%A, %B %e, %Y")
+  end
+
+  def stringified_time
+    string = self.time
+    string.strftime("%l:%M %p")
+  end
 end
--- app/serializers/appointment_serializer.rb
+++ app/serializers/appointment_serializer.rb
@@ -1,5 +1,5 @@
 class AppointmentSerializer < ActiveModel::Serializer
-  attributes :id, :diagnosis, :directions, :date, :time
+  attributes :id, :diagnosis, :directions, :date, :time, :stringified_date, :stringified_time
     belongs_to :doctor
     belongs_to :patient
 
