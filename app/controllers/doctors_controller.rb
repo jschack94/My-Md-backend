97e62f0c22d589c2d1f3036ff556a03a27cc5153
Jonathan <github email address>
2020-03-03 14:25:05 -0600

    no real chances made

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -14,9 +14,6 @@ class DoctorsController < ApplicationController
     doctor = Doctor.find_or_create_by(email: params[:email])
     render json: doctor
   end
-<<<<<<< HEAD
   
-=======
 
->>>>>>> d3ff470e6af004b04115c96fa835bb370729d93e
 end
