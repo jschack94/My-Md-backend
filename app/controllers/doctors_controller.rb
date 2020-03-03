cf0d91bba4956e4539134a0fd33a46b584f505a9
Jonathan <github email address>
2020-03-03 11:43:21 -0600

    new

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,8 +1,25 @@
 class DoctorsController < ApplicationController
+<<<<<<< HEAD
     
     def index
         doctors = Doctor.all
         render json: doctors
       end
 
+=======
+
+  def index
+    doctors = Doctor.all
+    render json: doctors
+  end
+
+  def show
+    doctor = Doctor.find_by(id: params[:id])
+    render json: doctor
+  end
+
+  def create
+    doctor = Doctor.find_or_create_by(email: params[:email])
+  end
+>>>>>>> b65ea04acc212170abdec2dd26afd0064a148f73
 end
