36d50109ea6a87bb9a0aa3090259bf6d4bb09e9d
Jonathan <github email address>
2020-03-03 11:40:57 -0600

    new

--- app/serializers/doctor_serializer.rb
+++ app/serializers/doctor_serializer.rb
@@ -1,6 +1,5 @@
 class DoctorSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :specialty, :degree, :bio, :residency, :email, :image, :full_name
-
   # def full_name
   #   object.first_name + object.last_name
   # end
