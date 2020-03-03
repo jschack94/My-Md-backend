05a1f51a63e400d01639a6c5be449c9682f474c3
John Leavell <johnleavell@gmail.com>
2020-03-03 12:58:33 -0600

    added update method in appointments controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -10,4 +10,16 @@ def show
   render json: appointment
 end
 
+def update
+  appointment = Appointment.find_by(id: params[:id])
+  appointment.update(diagnosis: params[:diagnosis])
+  appointment.update(directions: params[:directions])
+
+  render json: appointment
+end
+
+# def edit
+#   appointment = Appointment.find(params[:id])
+# end
+
 end
