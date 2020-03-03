85eba7e137f7f4423e05fb2caf7e3e3e4a802e37
Lindsay Mecher <lindsaymecher@gmail.com>
2020-03-03 11:32:55 -0600

    added controller actions for doctors_controller

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,16 @@
 class DoctorsController < ApplicationController
+
+  def index
+    doctors = Doctor.all
+    render json: doctors
+  end
+
+  def show
+    doctor = Doctor.find_by(id: params[:id])
+    render json: doctor
+  end
+
+  def create
+    doctor = Doctor.find_or_create_by(email: params[:email])
+  end
 end
